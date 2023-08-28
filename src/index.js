77d497a7ae2e635d41acb565906906c8315f67db
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-08-28 19:55:59 +0300

    Fix vs code bug

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+          <BrowserRouter>
             <App />
           </BrowserRouter>
         </PersistGate>
